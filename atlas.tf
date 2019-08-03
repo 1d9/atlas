2d9efbc1f190e3f00ac95ca555585fb921d469a6
Luke Kaalim <lukekaalim@gmail.com>
2019-08-04 02:28:09 +1000

    Copy artefacts to directory

--- atlas.tf
+++ atlas.tf
@@ -18,8 +18,8 @@ resource "aws_s3_bucket" "beanstalk-bucket" {
   }
 }
 
-resource "aws_s3_bucket_object" "object" {
-  bucket = "{aws_s3_bucket.beanstalk-bucket.id}"
+resource "aws_s3_bucket_object" "application-v1" {
+  bucket = "${aws_s3_bucket.beanstalk-bucket.id}"
   key    = "applications/v1.zip"
   source = "artefacts/cartographer.zip"
 }
