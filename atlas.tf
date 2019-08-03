83bed40f971c54c9a7de06c4551cfbb1c3cae039
Luke Kaalim <lukekaalim@gmail.com>
2019-08-04 02:35:23 +1000

    Add elastic beanstalk

--- atlas.tf
+++ atlas.tf
@@ -18,8 +18,21 @@ resource "aws_s3_bucket" "beanstalk-bucket" {
   }
 }
 
-resource "aws_s3_bucket_object" "application-v1" {
+resource "aws_s3_bucket_object" "application-object" {
   bucket = "${aws_s3_bucket.beanstalk-bucket.id}"
   key    = "applications/v1.zip"
   source = "artefacts/cartographer.zip"
+}
+
+resource "aws_elastic_beanstalk_application" "cartographer-app" {
+  name        = "1d9-cartographer"
+  description = "1d9 Astral Atlas Cartographer application"
+}
+
+resource "aws_elastic_beanstalk_application_version" "v1" {
+  name        = "v1"
+  application = "1d9-cartographer"
+  description = "application version created by terraform"
+  bucket      = "${aws_s3_bucket.beanstalk-bucket.id}"
+  key         = "${aws_s3_bucket_object.application-object.id}"
 }
