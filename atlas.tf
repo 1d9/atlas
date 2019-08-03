5a955ffd21dfa94153c258927a3d57b485632006
Luke Kaalim <lukekaalim@gmail.com>
2019-08-04 02:08:42 +1000

    Edit atlas to properly name bucket variable

--- atlas.tf
+++ atlas.tf
@@ -8,7 +8,7 @@ resource "aws_s3_bucket" "cartographer-bucket" {
   }
 }
 
-resource "aws_s3_bucket" "1d9-beanstalk-applications" {
+resource "aws_s3_bucket" "beanstalk-bucket" {
   bucket = "1d9-beanstalk-applications"
   acl    = "private"
 
@@ -19,7 +19,7 @@ resource "aws_s3_bucket" "1d9-beanstalk-applications" {
 }
 
 resource "aws_s3_bucket_object" "object" {
-  bucket = "{aws_s3_bucket.1d9-beanstalk-applications.id}"
-  key    = "application/v1"
+  bucket = "{aws_s3_bucket.beanstalk-bucket.id}"
+  key    = "applications/v1.zip"
   source = "artefacts/cartographer.zip"
 }
