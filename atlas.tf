99bbeb0a3bdc917f8c391a18bc2e127960cbd68f
Luke Kaalim <lukekaalim@gmail.com>
2019-08-21 01:08:33 +1000

    Add staging environment

--- atlas.tf
+++ atlas.tf
@@ -22,3 +22,23 @@ resource "aws_elastic_beanstalk_environment" "cartographer-production" {
     value     = "arn:aws:iam::420148351138:instance-profile/AstralAtlasEC2Role"
   }
 }
+
+resource "aws_elastic_beanstalk_environment" "cartographer-staging" {
+  name                = "cartographer-staging"
+  application         = "${aws_elastic_beanstalk_application.cartographer-app.name}"
+  version_label       = "v0.0.4"
+  solution_stack_name = "64bit Amazon Linux 2018.03 v2.12.14 running Docker 18.06.1-ce"
+
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "CARTOGRAPHER_CONFIG_PATH"
+    value     = "/opt/cartographer/staging.json"
+  }
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    # Hardcoded value
+    value     = "arn:aws:iam::420148351138:instance-profile/AstralAtlasEC2Role"
+  }
+}
