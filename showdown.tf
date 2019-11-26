f0947d0d7eecd5956adf548a605bf3069d793318
Luke Kaalim <lukekaalim@gmail.com>
2019-11-27 02:40:13 +1100

    Add showdown config and update

--- showdown.tf
+++ showdown.tf
@@ -11,9 +11,24 @@ module "showdown-s3" {
   source = "./github-release-to-s3"
 
   github_repo_name = "1d9/showdown"
-  github_repo_release = "v3.0.2"
+  github_repo_release = "v3.1.0"
   github_repo_release_filename = "dist.zip"
   bucket_name = "showdown.1d9.tech"
+}
+
+resource "aws_s3_bucket_object" "showdown_config" {
+  acl = "public-read"
+  bucket = aws_s3_bucket.showdown.bucket
+  key = "config.json"
+
+  content = <<EOT
+  {
+    "atlasEndpoint": "http://api.tome.1d9.tech",
+    "showdownSessionId": "4839c4c3-2dc4-47ce-959b-b489253c348c"
+  }
+  EOT
+
+  content_type = "application/json"
 }
 
 resource "aws_route53_record" "showdown-record" {
